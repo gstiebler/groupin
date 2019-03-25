bc79a0b25fbe21fcada6cf03ec287eb3641598ce
Guilherme Stiebler <guilhermemst@gmail.com>
2019-03-25 18:10:49 -0300

    ios reset

--- mobile/ios/GroupInTests/GroupInTests.m
+++ mobile/ios/GroupInTests/GroupInTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface mobileRNCTests : XCTestCase
+@interface GroupInTests : XCTestCase
 
 @end
 
-@implementation mobileRNCTests
+@implementation GroupInTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
