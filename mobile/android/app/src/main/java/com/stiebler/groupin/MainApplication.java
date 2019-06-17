e9407aa3f1567ac46a65914e06506cb0148e4b2c
Guilherme Stiebler <guilhermemst@gmail.com>
2019-06-16 23:11:13 -0300

    Added Firebase/Auth to Android

--- mobile/android/app/src/main/java/com/stiebler/groupin/MainApplication.java
+++ mobile/android/app/src/main/java/com/stiebler/groupin/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
+              new RNFirebaseAuthPackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
             new VectorIconsPackage()
