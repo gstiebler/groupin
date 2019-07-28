0e30c181b80b7771e70f26af31ca0dd27f5f71a5
Guilherme Stiebler <guilhermemst@gmail.com>
2019-07-28 17:39:29 -0300

    Reworked graphqlMain

--- server/src/graphqlMain.js
+++ server/src/graphqlMain.js
@@ -6,14 +6,17 @@ const User = require('./db/schema/User');
 const _ = require('lodash');
 
 async function main(graphqlQuery, firebaseToken) {
-  let uid = null;
-  if (firebaseToken) {
-    // idToken comes from the client app  
-    const decodedToken = await admin.auth().verifyIdToken(firebaseToken);
-    uid = decodedToken.uid;
-  }
   // TODO: return only basic fields from the user, review all places that reads user.groups
-  const user = _.isEmpty(uid) ? null : await User.findOne({ uid });
+  let user = null;
+  if (firebaseToken) {
+    user = await User.findOne({ fcmToken: firebaseToken });
+    if (!user) {
+      // idToken comes from the client app  
+      const decodedToken = await admin.auth().verifyIdToken(firebaseToken);
+      const uid = decodedToken.uid;
+      user = _.isEmpty(uid) ? null : await User.findOne({ uid });
+    }
+  }
   const result = await graphql(schema, graphqlQuery, null, { user });
   return result;
 }
