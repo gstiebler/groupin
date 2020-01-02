7da7979760418212e1a9f3fe9ea438001c811c23
Guilherme Stiebler <guilhermemst@gmail.com>
2020-01-02 19:11:42 -0300

    Fixed background data message on Android

--- mobile/index.js
+++ mobile/index.js
@@ -7,3 +7,8 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+// Only to avoid crashing on data messages when in background
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', async () => { 
+  console.log('** RNFirebaseBackgroundMessage');
+  return Promise.resolve();
+});
