761b7ba91017ae8279d606ccdca64d6399ed924c
Guilherme Stiebler <guilhermemst@gmail.com>
2020-08-19 17:58:03 -0300

    Restored store

---
+++ mobile/store/rootStore.js
@@ -0,0 +1,19 @@
+const { createStore, applyMiddleware } = require("redux");
+const rootReducer = require("../reducers/rootReducer");
+const { composeWithDevTools } = require('remote-redux-devtools');
+const thunk = require('redux-thunk').default;
+
+const store = createStore(
+  rootReducer, 
+  {},
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+if (module.hot) {
+  module.hot.accept(() => {
+    const nextRootReducer = require('../reducers/rootReducer');
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+module.exports = store;
