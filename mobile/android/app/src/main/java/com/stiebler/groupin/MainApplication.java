c89e3f278e5d8ce7e3c85d964231a55276ad958e
Guilherme Stiebler <guilhermemst@gmail.com>
2020-08-19 18:53:13 -0300

    Renamed app in Android

--- mobile/android/app/src/main/java/com/stiebler/groupin/MainApplication.java
+++ mobile/android/app/src/main/java/com/stiebler/groupin/MainApplication.java
@@ -1,4 +1,4 @@
-package com.groupin;
+package com.stiebler.groupin;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.groupin.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.stiebler.groupin.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
