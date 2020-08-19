fd2cca6378f62b0ae13ec5027e5834167649d97e
Guilherme Stiebler <guilhermemst@gmail.com>
2020-08-19 17:12:17 -0300

    Deleted root files

---
+++ mobile/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // SERVER_URL: 'https://api.gstiebler.net/graphql',
+  SERVER_URL: 'http://10.0.2.2:3000/dev/graphql', // Android
+  // SERVER_URL: 'http://localhost:3000/dev/graphql', // iOS
+};
