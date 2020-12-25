80cbd7a85ae1810d72219471efb36b7660e646a6
Guilherme Stiebler <guilhermemst@gmail.com>
2020-12-25 16:58:36 -0300

    merge

--- server/src/graphqlSchema.ts
+++ server/src/graphqlSchema.ts
@@ -2,7 +2,7 @@ import { buildSchema } from 'graphql';
 import fs = require('fs');
 import path = require('path');
 
-const gqlPath = path.join(__dirname, '../src/schema.gql');
+const gqlPath = path.join(__dirname, './schema.gql');
 const schema = buildSchema(fs.readFileSync(gqlPath, 'utf8'));
 
 export default schema;
