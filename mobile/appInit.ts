75dba0f541eb7f86e7fbd294d7ed72b4829d9376
Guilherme Stiebler <guilhermemst@gmail.com>
2021-06-27 19:17:49 -0300

    fixed AppInit

--- mobile/appInit.ts
+++ mobile/appInit.ts
@@ -1,11 +1,7 @@
 // import _ from 'lodash';
 import * as FCM from './lib/fcm';
-import * as loginActions from './actions/loginActions';
-import store from "./store/rootStore";
 import { NavFn } from './components/Navigator';
 
 export default async function init(navigate: NavFn) {
-  const dispatch = store.dispatch.bind(store);
-  await loginActions.init(navigate, dispatch);
   await FCM.init(navigate);
 }
