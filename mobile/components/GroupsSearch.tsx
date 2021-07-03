cad4aaa76333efc16e23d0b40427813312b49069
Guilherme Stiebler <guilhermemst@gmail.com>
2021-07-03 15:24:55 -0300

    removed search box

--- mobile/components/GroupsSearch.tsx
+++ mobile/components/GroupsSearch.tsx
@@ -7,8 +7,8 @@ import {
   Text,
   Content,
 } from 'native-base';
-import Search from 'react-native-search-box';
 import { Group } from '../lib/server';
+import { TextInput } from 'react-native';
 
 interface GroupSearchProps {
   groups: Group[];
@@ -34,13 +34,8 @@ const GroupSearchComponent: React.FC<GroupSearchProps> = ({
   return (
     <Container>
       <Content>
-        <Search
-          cancelButtonWidth={80}
-          onSearch={changeSearchText}
+        <TextInput
           onChangeText={changeSearchText}
-          onCancel={reset}
-          onDelete={reset}
-          cancelTitle='Cancelar'
           placeholder='Buscar'
         />
         <List 
