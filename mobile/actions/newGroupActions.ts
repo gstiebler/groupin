64a2c9124a560a96407471a0b843a408a1208020
Guilherme Stiebler <guilhermemst@gmail.com>
2021-07-03 11:06:20 -0300

    removed extra function

--- mobile/actions/newGroupActions.ts
+++ mobile/actions/newGroupActions.ts
@@ -1,7 +1,7 @@
 import { Navigation } from '../components/Navigator';
 import * as server from '../lib/server';
 
-export const createGroup = (params: { navigation: Navigation, groupName: string, visibility: string }) => async () => {
+export const createGroup = async (params: { navigation: Navigation, groupName: string, visibility: string }) => {
   const { navigation, groupName, visibility } = params;
   await server.createGroup({ groupName, visibility });
   navigation.goBack();
