6061ea0a7aaace6139b1e014176a5dadb4c2cbf6
Guilherme Stiebler <guilhermemst@gmail.com>
2021-07-17 20:15:56 -0300

    removed file

--- server/src/graphqlSchema.ts
+++
@@ -1,8 +0,0 @@
-import { buildSchema } from 'graphql';
-import * as fs from 'fs';
-import * as path from 'path';
-
-const gqlPath = path.join(__dirname, '../src/schema.gql');
-const schema = buildSchema(fs.readFileSync(gqlPath, 'utf8'));
-
-export default schema;
