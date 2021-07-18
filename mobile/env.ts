00b6054ba66db7e004974c4bcb3525670c8f49a1
Guilherme Stiebler <guilhermemst@gmail.com>
2021-07-17 23:20:48 -0300

    ts

--- mobile/env.ts
+++ mobile/env.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  SERVER_URL: 'https://api.gstiebler.net/graphql',
+export default {
+  // SERVER_URL: 'https://api.gstiebler.net/graphql',
   // SERVER_URL: 'http://10.0.2.2:3000/dev/graphql', // Android
   // SERVER_URL: 'http://localhost:3000/dev/graphql', // iOS
+  SERVER_URL: 'http://localhost:7071/api/HttpTrigger1'
 };
--- mobile/manualTest.ts
+++ mobile/manualTest.ts
@@ -1,9 +1,9 @@
-const server = require('./lib/server');
-const { setToken } = require('./lib/graphqlConnect');
+import * as server from './lib/server';
+import { setToken } from './lib/graphqlConnect';
 
 const randomStr = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
-(async function test() {
+async function test() {
   try {
     setToken('eyJhbGciOiJSUzI1NiIsImtpZCI6ImNlNWNlZDZlNDBkY2QxZWZmNDA3MDQ4ODY3YjFlZDFlNzA2Njg2YTAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZ3JvdXBpbi00NzAwYiIsImF1ZCI6Imdyb3VwaW4tNDcwMGIiLCJhdXRoX3RpbWUiOjE1Nzc5MTU5NDQsInVzZXJfaWQiOiJlWEpxMG9teUtGWTNwMzI5QlhCWmV5bDJRVjkzIiwic3ViIjoiZVhKcTBvbXlLRlkzcDMyOUJYQlpleWwyUVY5MyIsImlhdCI6MTU3NzkxNTk0NSwiZXhwIjoxNTc3OTE5NTQ1LCJwaG9uZV9udW1iZXIiOiIrNTUyMTk5OTk5MjIyMiIsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsicGhvbmUiOlsiKzU1MjE5OTk5OTIyMjIiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwaG9uZSJ9fQ.OT2liwKEJNN8jXECiIw9TR1MdDoTgmZB0jXCoKXvTj56S4v0g80Uo3NgycTarV6MIpFz55VrsEsA7DyDKU83GwCA0b246Z2eNx7wXE1nxodItZEozMXwXElWaqpt1NlCr_KjFqgs2KZdqQbQlFGCsvICkYcvuDZTC9wfssnrlQ_4j-lgK9BTLsX3AnqKWBFzL5JN-hwkiViUEZN6ScLOoa5gC3tHpet0V7MWN4Q8epzNH8TX1ykIxjDrFf9WPBpCNFhMoqQiiq95WIBrS72zqdwPHoLku2OC-RiUTumoBxwmyzq1-hhLLCzPShC_OJwP2MXXoSxOC9va_aXYskYi9g');
     const message = `teste manual ${randomStr()}`;
@@ -12,4 +12,6 @@ const randomStr = () => Math.random().toString(36).substring(2, 15) + Math.rando
   } catch(error) {
     console.error(error);
   }
-})();
+}
+
+test();
