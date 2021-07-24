c11ad418932b58310cb5d1c827f0c9fafc4eca1f
Guilherme Stiebler <guilhermemst@gmail.com>
2021-07-24 15:01:54 -0300

    migration for initial schema

---
+++ server/src/db/migration/1627149566710-FirstMigration.ts
@@ -0,0 +1,48 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class FirstMigration1627149566710 implements MigrationInterface {
+    name = 'FirstMigration1627149566710'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "pinned_topic" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, "topicId" uuid, CONSTRAINT "PK_5ee5d46d5c00581d3bf5bb057a4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "topic_latest_read" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "latestMoment" TIMESTAMP NOT NULL, "userId" uuid, "topicId" uuid, CONSTRAINT "PK_1741172ac1a88119525b7c689fd" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user_group" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "latestRead" TIMESTAMP NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, "groupId" uuid, CONSTRAINT "PK_3c29fba6fe013ec8724378ce7c9" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "externalId" character varying NOT NULL, "notificationToken" character varying NOT NULL, "imgUrl" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "message" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "text" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, "topicId" uuid, CONSTRAINT "PK_ba01f0a3e0123651915008bc578" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "topic" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "imgUrl" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "groupId" uuid, "createdById" uuid, CONSTRAINT "PK_33aa4ecb4e4f20aa0157ea7ef61" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "group" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "friendlyId" character varying NOT NULL, "imgUrl" character varying, "description" character varying NOT NULL, "visibility" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "createdById" uuid, CONSTRAINT "PK_256aa0fda9b1de1a73ee0b7106b" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "pinned_topic" ADD CONSTRAINT "FK_affbfc7f3679927a2e4207cb83c" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "pinned_topic" ADD CONSTRAINT "FK_0ad0a6eb6e3bdecfd7463ee27cf" FOREIGN KEY ("topicId") REFERENCES "topic"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "topic_latest_read" ADD CONSTRAINT "FK_fd93b28b56ad61b5e40d089a8f5" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "topic_latest_read" ADD CONSTRAINT "FK_616dee84babda2acd743e9b3a68" FOREIGN KEY ("topicId") REFERENCES "topic"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "user_group" ADD CONSTRAINT "FK_3d6b372788ab01be58853003c93" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "user_group" ADD CONSTRAINT "FK_31e541c93fdc0bb63cfde6549b7" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "message" ADD CONSTRAINT "FK_446251f8ceb2132af01b68eb593" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "message" ADD CONSTRAINT "FK_ba61dccaec11043afd95302f254" FOREIGN KEY ("topicId") REFERENCES "topic"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "topic" ADD CONSTRAINT "FK_3a75f2197bbc84974ac2aba1e57" FOREIGN KEY ("groupId") REFERENCES "topic"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "topic" ADD CONSTRAINT "FK_59d7548ea797208240417106e2d" FOREIGN KEY ("createdById") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "group" ADD CONSTRAINT "FK_5a1ceb121c801a21673ef1b3f36" FOREIGN KEY ("createdById") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "group" DROP CONSTRAINT "FK_5a1ceb121c801a21673ef1b3f36"`);
+        await queryRunner.query(`ALTER TABLE "topic" DROP CONSTRAINT "FK_59d7548ea797208240417106e2d"`);
+        await queryRunner.query(`ALTER TABLE "topic" DROP CONSTRAINT "FK_3a75f2197bbc84974ac2aba1e57"`);
+        await queryRunner.query(`ALTER TABLE "message" DROP CONSTRAINT "FK_ba61dccaec11043afd95302f254"`);
+        await queryRunner.query(`ALTER TABLE "message" DROP CONSTRAINT "FK_446251f8ceb2132af01b68eb593"`);
+        await queryRunner.query(`ALTER TABLE "user_group" DROP CONSTRAINT "FK_31e541c93fdc0bb63cfde6549b7"`);
+        await queryRunner.query(`ALTER TABLE "user_group" DROP CONSTRAINT "FK_3d6b372788ab01be58853003c93"`);
+        await queryRunner.query(`ALTER TABLE "topic_latest_read" DROP CONSTRAINT "FK_616dee84babda2acd743e9b3a68"`);
+        await queryRunner.query(`ALTER TABLE "topic_latest_read" DROP CONSTRAINT "FK_fd93b28b56ad61b5e40d089a8f5"`);
+        await queryRunner.query(`ALTER TABLE "pinned_topic" DROP CONSTRAINT "FK_0ad0a6eb6e3bdecfd7463ee27cf"`);
+        await queryRunner.query(`ALTER TABLE "pinned_topic" DROP CONSTRAINT "FK_affbfc7f3679927a2e4207cb83c"`);
+        await queryRunner.query(`DROP TABLE "group"`);
+        await queryRunner.query(`DROP TABLE "topic"`);
+        await queryRunner.query(`DROP TABLE "message"`);
+        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`DROP TABLE "user_group"`);
+        await queryRunner.query(`DROP TABLE "topic_latest_read"`);
+        await queryRunner.query(`DROP TABLE "pinned_topic"`);
+    }
+
+}
