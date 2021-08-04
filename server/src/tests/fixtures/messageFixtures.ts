6b7350cbc4d3372760bb5febde08e4c6a7856795
Guilherme Stiebler <guilhermemst@gmail.com>
2021-08-04 09:26:39 -0300

    loading fixtures :)

--- server/src/tests/fixtures/messageFixtures.ts
+++ server/src/tests/fixtures/messageFixtures.ts
@@ -1,6 +1,5 @@
-import userFixtures from './userFixtures';
 import { convertFixedDate } from './fixtureHelper';
-import { topicIds } from './preIds';
+import { topicIds, userIds } from './preIds';
 import { Message } from '../../db/schema/Message';
 import { Types } from 'mongoose';
 const { ObjectId } = Types;
@@ -9,28 +8,28 @@ const messages: { [message: string]: Partial<Message> } = {
   message1topic1: {
     _id: new ObjectId(),
     text: 'Topic 1 Group 1 Alice',
-    userId: userFixtures.alice.id,
+    userId: userIds.alice,
     topicId: topicIds.topic1Group1,
     createdAt: convertFixedDate('2018-10-01'),
   },
   message2topic1: {
     _id: new ObjectId(),
     text: 'Topic 1 Group 1 Robert',
-    userId: userFixtures.robert.id,
+    userId: userIds.robert,
     topicId: topicIds.topic1Group1,
     createdAt: convertFixedDate('2018-10-02'),
   },
   message1topic2: {
     _id: new ObjectId(),
     text: 'Topic 2 Group 1 Alice',
-    userId: userFixtures.alice.id,
+    userId: userIds.alice,
     topicId: topicIds.topic2Group1,
     createdAt: convertFixedDate('2018-10-03'),
   },
   message1topic1Group2: {
     _id: new ObjectId(),
     text: 'Topic 1 Group 2 Robert elvis',
-    userId: userFixtures.robert.id,
+    userId: userIds.robert,
     topicId: topicIds.topic1Group2,
     createdAt: convertFixedDate('2018-09-12'),
   },
