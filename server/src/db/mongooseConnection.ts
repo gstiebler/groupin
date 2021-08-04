1637ef03f8f1001cab16a606042259c8dd346ab0
Guilherme Stiebler <guilhermemst@gmail.com>
2021-08-04 09:21:47 -0300

    removed warnings

--- server/src/db/mongooseConnection.ts
+++ server/src/db/mongooseConnection.ts
@@ -2,13 +2,14 @@ import * as mongoose from 'mongoose';
 import logger from '../config/winston';
 
 export async function createMongooseConnection(mongoURL: string): Promise<typeof mongoose | undefined> {
+  mongoose.set('useCreateIndex', true);
   mongoose.set('useFindAndModify', false);
   mongoose.set('bufferCommands', false);
 
   await mongoose.disconnect();
 
   try {
-    const db = await mongoose.connect(mongoURL);
+    const db = await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
     logger.info('Connected to MongoDB');
     return db;
   } catch (error) {
