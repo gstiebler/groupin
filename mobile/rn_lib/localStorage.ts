b7df6b6e3a71e7134933efd53c1795aecf256cf2
Guilherme Stiebler <guilhermemst@gmail.com>
2021-08-07 11:48:12 -0300

    updated types on local storage

--- mobile/rn_lib/localStorage.ts
+++ mobile/rn_lib/localStorage.ts
@@ -1,13 +1,14 @@
 import { AsyncStorage } from 'react-native'; // TODO: replace by @react-native-async-storage/async-storage
+import { GiMessage } from '../lib/messages';
 import { IStorage } from '../types/Storage.types';
 
 export class LocalStorage implements IStorage {
-  async getItem(key: string): Promise<unknown> {
+  async getMessages(key: string): Promise<GiMessage[]> {
     const res = await AsyncStorage.getItem(key);
     return res ? JSON.parse(res) : null;
   }
   
-  async setItem(key: string, value: unknown) {
+  async setMessages(key: string, value: GiMessage[]) {
     await AsyncStorage.setItem(key, JSON.stringify(value));
   }
 }
