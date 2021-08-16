c90eb1b1fbfe1aef966e3e0598be93d10763baa6
Guilherme Stiebler <gms.public@stiebler.me>
2021-08-15 22:23:56 -0300

    Updated group list container to be MobX observer

--- mobile/containers/GroupListContainer.tsx
+++ mobile/containers/GroupListContainer.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import GroupListComponent from '../components/GroupList';
 import { useEffect } from 'react';
-import { groupStore } from '../rn_lib/storesFactory';
+import { groupStore as groupStoreInstance } from '../rn_lib/storesFactory';
 import { Group } from '../lib/server';
 import { Navigation } from '../rn_lib/Navigator.types';
+import { observer } from "mobx-react-lite"
+import { GroupStore } from '../stores/groupStore';
 
-const GroupListContainer: React.FC<{ navigation: Navigation }> = ({ navigation }) => {
+const GroupListContainerObserver: React.FC<{ navigation: Navigation, groupStore: GroupStore }> = observer(({ navigation, groupStore }) => {
   const willFocus = () => groupStore.fetchOwnGroups();
   useEffect(() => navigation.addListener('focus', willFocus), [navigation]);
 
@@ -17,5 +20,8 @@ const GroupListContainer: React.FC<{ navigation: Navigation }> = ({ navigation }
     }, 
     onPinClicked: (group: Group) => groupStore.setGroupPin({ groupId: group.id, pinned: !group.pinned }),
   });
-};
+});
+const GroupListContainer: React.FC<{ navigation: Navigation }> = ({ navigation }) => (
+  <GroupListContainerObserver groupStore={groupStoreInstance} navigation={navigation}/>
+);
 export default GroupListContainer;
