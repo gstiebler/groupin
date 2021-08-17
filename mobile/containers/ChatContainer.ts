94823b768bfc70843f633af6d2a39547421289ea
Guilherme Stiebler <gms.public@stiebler.me>
2021-08-16 21:15:54 -0300

    updated chat container for reactivity

--- mobile/containers/ChatContainer.ts
+++ mobile/containers/ChatContainer.ts
@@ -1,14 +1,16 @@
 import { localStorage as storage } from '../rn_lib/localStorage';
-import { rootStore } from '../rn_lib/storesFactory';
+import { rootStore as rootStoreInstance } from '../rn_lib/storesFactory';
 import ChatComponent from '../components/Chat';
 import React, { useEffect } from 'react';
 import { Navigation, RootStackParamList } from '../rn_lib/Navigator.types';
 import { RouteProp } from '@react-navigation/native';
 import { notifications } from '../rn_lib/notifications';
+import { observer } from 'mobx-react-lite';
+import { RootStore } from '../stores/rootStore';
 
 export type ChatScreenRouteProp = RouteProp<RootStackParamList, 'Chat'>;
-type ContainerProp = { navigation: Navigation, route: ChatScreenRouteProp };
-const ChatContainer: React.FC<ContainerProp> = ({ navigation, route }) => {
+type ContainerProp = { navigation: Navigation, route: ChatScreenRouteProp, rootStore: RootStore };
+const ChatContainerObserver: React.FC<ContainerProp> = observer(({ navigation, route, rootStore }) => {
   const willFocus = () => {
     rootStore.topicStore.onTopicOpened({ 
       topicId: route.params.topicId, 
@@ -34,5 +36,8 @@ const ChatContainer: React.FC<ContainerProp> = ({ navigation, route }) => {
     onSend: messages => rootStore.sendMessages(messages),
     onLoadEarlier: () => rootStore.onOlderMessagesRequested(route.params.topicId),
   })
-};
+}); 
+const ChatContainer: React.FC<{ navigation: Navigation, route: ChatScreenRouteProp }> = ({ navigation, route }) => (
+  <ChatContainerObserver navigation={navigation} route={route} rootStore={rootStoreInstance} />
+);
 export default ChatContainer;
