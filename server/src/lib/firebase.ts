c56dbc5aced8ec6a54265a937a5346199e96fd9b
Guilherme Stiebler <gms.public@stiebler.me>
2021-09-12 01:25:46 -0300

    convert line breaks for firebase private key

--- server/src/lib/firebase.ts
+++ server/src/lib/firebase.ts
@@ -2,10 +2,12 @@ import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 import { envConfig } from '../config/envConfig';
 
+const convertLineBreaks = (input: string) => input.split('\\n').join('\n');
+
 const serviceAccount: ServiceAccount = {
   projectId: "groupin-stiebler",
   clientEmail: "firebase-adminsdk-6ex1x@groupin-stiebler.iam.gserviceaccount.com",
-  privateKey: envConfig.FIREBASE_PRIVATE_KEY,
+  privateKey: convertLineBreaks(envConfig.FIREBASE_PRIVATE_KEY!),
 };
 const config = {
   credential: admin.credential.cert(serviceAccount),
